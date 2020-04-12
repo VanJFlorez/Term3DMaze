6babc004d5c9c102db00880fcfb4e9582c892aea
Juan Camilo <jcflorezv@unal.edu.co>
2020-04-11 20:04:02 -0500

    In front of the first wall

--- Term3DMaze/Term3DMaze.cpp
+++ Term3DMaze/Term3DMaze.cpp
@@ -2,10 +2,104 @@
 //
 
 #include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
 
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f; // The angle a player is looking at
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFOV = 3.14159/4.0f; // field of view 45°
+float fDepth = 16.0f; 
+
+// engine loop
 int main()
 {
-    std::cout << "Hello World!\n";
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    wstring map;
+             
+    map += L"################";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"################";
+
+
+    while (1) 
+    {
+        for (int x = 0; x < nScreenWidth; x++) {
+            float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            // compute the fDistanceToWall from the current's player position and the current 
+            // column ray computation.
+            while (!bHitWall && fDistanceToWall < fDepth) {
+                fDistanceToWall += 0.1f;
+                
+                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+                if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+                {
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                }
+                else
+                {
+                    if (map[nTestY * nMapWidth + nTestX] == '#')
+                    {
+                        bHitWall = true;
+                    }
+                }
+            }
+
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+
+            for (int y = 0; y < nScreenHeight; y++)
+            {
+                if (y < nCeiling)
+                    screen[y * nScreenWidth + x] = ' '; // draw this px as empty char
+                else if (y > nCeiling && y <= nFloor)
+                    screen[y * nScreenWidth + x] = '#'; // draw this px as a sharp
+                else {
+                    screen[y * nScreenWidth + x] = ' '; // draw floor
+                }
+            }
+
+        }
+
+        screen[nScreenWidth * nScreenHeight - 1] = '\0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0, 0 }, &dwBytesWritten);
+    }
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
